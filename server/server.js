fee8c7db039f9c4dd472e1a7bfcfc69de4a9f2bd
Timothy Attah <timothyattah733@gmail.com>
2024-03-11 14:56:42 +0100

    Fixing cors issues

--- server/server.js
+++ server/server.js
@@ -12,10 +12,28 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  // origin: 'http://localhost:3000',
+  origin: 'https://enoch-louis-bootcamp.vercel.app/',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Welcome To Enoch Louis Coding Bootcamp School');
+});
+
+router.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '1800');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'PUT, POST, GET, DELETE, PATCH, OPTIONS',
+  );
 });
 
 app.use('/api/auth', authRouters);
